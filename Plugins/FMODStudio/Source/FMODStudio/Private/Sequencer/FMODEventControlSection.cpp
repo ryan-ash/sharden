1f0f8ba4aeddcf01c9b71703534cf531d2449b3c
Ash <scarletspark@gmail.com>
2021-01-29 22:15:37 +0300

    fmod plugin added

---
+++ Plugins/FMODStudio/Source/FMODStudio/Private/Sequencer/FMODEventControlSection.cpp
@@ -0,0 +1,50 @@
+// Copyright (c), Firelight Technologies Pty, Ltd. 2012-2020.
+
+#include "FMODEventControlSection.h"
+#include "FMODEventControlSectionTemplate.h"
+#include "Channels/MovieSceneChannelProxy.h"
+#include "UObject/SequencerObjectVersion.h"
+#include "UObject/Package.h"
+
+FFMODEventControlChannel::FFMODEventControlChannel()
+{
+    SetEnum(FindObject<UEnum>(ANY_PACKAGE, TEXT("EFMODEventControlKey")));
+}
+
+UFMODEventControlSection::UFMODEventControlSection(const FObjectInitializer &ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    SetRange(TRange<FFrameNumber>::All());
+
+    int32 LinkerCustomVersion = GetLinkerCustomVersion(FSequencerObjectVersion::GUID);
+    EMovieSceneCompletionMode CompletionMode;
+
+    if (LinkerCustomVersion < FSequencerObjectVersion::WhenFinishedDefaultsToRestoreState)
+    {
+        CompletionMode = EMovieSceneCompletionMode::KeepState;
+    }
+    else if (LinkerCustomVersion < FSequencerObjectVersion::WhenFinishedDefaultsToProjectDefault)
+    {
+        CompletionMode = EMovieSceneCompletionMode::RestoreState;
+    }
+    else
+    {
+        CompletionMode = EMovieSceneCompletionMode::ProjectDefault;
+    }
+
+#if WITH_EDITOR
+
+    ChannelProxy = MakeShared<FMovieSceneChannelProxy>(ControlKeys, FMovieSceneChannelMetaData(), TMovieSceneExternalValue<uint8>());
+
+#else
+
+    ChannelProxy = MakeShared<FMovieSceneChannelProxy>(ControlKeys);
+
+#endif
+}
+
+// ash: temporary crutch to compile @ 4.26
+// FMovieSceneEvalTemplatePtr UFMODEventControlSection::GenerateTemplate() const
+// {
+//     return FFMODEventControlSectionTemplate(*this);
+// }
