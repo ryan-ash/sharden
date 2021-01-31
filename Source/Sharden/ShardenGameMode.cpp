65658d03e3ba8bdff164905cd7b43d803423bf90
Sashovskiy <oleksandr126@gmail.com>
2021-01-31 16:54:56 +0300

    Merge branch 'main' of https://github.com/ryan-ash/jammy-21

--- Source/Sharden/ShardenGameMode.cpp
+++ Source/Sharden/ShardenGameMode.cpp
@@ -7,15 +7,22 @@
 void AShardenGameMode::Tick_Impl(float DeltaSeconds)
 {
     SpawnTime += DeltaSeconds;
+    PlayTime += DeltaSeconds;
 
     if (!CurrentSpawnParameters)
     {
         UE_LOG(LogTemp, Error, TEXT("Spawn parameters nullptr!"));
         return;
     }
-    if (SpawnTime > CurrentSpawnParameters->SpawnTime)
+
+    if (PlayTime > CurrentSpawnParameters->PlayTime)
     {
-        SpawnTime /= CurrentSpawnParameters->SpawnTime;
+        // TODO: Win
+    }
+    const float DifficultyValue = CurrentSpawnParameters->DifficultyCurve->GetFloatValue(PlayTime);
+    if (SpawnTime > CurrentSpawnParameters->SpawnTime / DifficultyValue)
+    {
+        SpawnTime = 0.0f;
         SpawnObstacle();
     }
 }
@@ -33,6 +40,7 @@ void AShardenGameMode::RegisterPlayerHit()
 
 void AShardenGameMode::PlayStart()
 {
+    PlayTime = 0.0f;
     SetObstaclesSpawnable(true);
 }
 
@@ -64,18 +72,24 @@ void AShardenGameMode::SpawnObstacle()
 
     for (int i = 0; i < SpawnCount; ++i)
     {
-        FActorSpawnParameters SpawnInfo;
-        const auto CurrentAngle = FMath::DegreesToRadians(FMath::RandRange(CurrentSpawnParameters->MinAngleDelta, CurrentSpawnParameters->MaxAngleDelta));
+        const int32 SelectedObstacleI = FMath::RandRange(0, CurrentSpawnParameters->Obstacles.Num() - 1);
+        const auto SelectedObstacle = CurrentSpawnParameters->Obstacles[SelectedObstacleI];
+        if (!CurrentSpawnParameters->Obstacles.Contains(SelectedObstacle))
+        {
+            UE_LOG(LogTemp, Error, TEXT("Obstacle doesn't have params struct!"));
+            return;
+        }
+        const auto ObstacleData = CurrentSpawnParameters->ObstaclesDataMap[SelectedObstacle];
+        const auto CurrentAngle = FMath::DegreesToRadians(FMath::RandRange(ObstacleData.MinAngleDelta, ObstacleData.MaxAngleDelta));
         const float X = GroundRadius * FMath::Cos(CurrentAngle);
         const float Z = GroundRadius * FMath::Sin(CurrentAngle);
-        const int32 SelectelObstacleI = FMath::RandRange(0, CurrentSpawnParameters->Obstacles.Num() - 1);
-        const auto SelectedObstacle = CurrentSpawnParameters->Obstacles[SelectelObstacleI];
+        FActorSpawnParameters SpawnInfo;
         const auto Obstacle = GetWorld()->SpawnActor<AObstacle>(SelectedObstacle, FVector(X, FMath::RandRange(-CurrentSpawnParameters->Width, CurrentSpawnParameters->Width), Z), FRotator(0, 0, 0), SpawnInfo);
-        Obstacle->SetActorScale3D(Obstacle->GetActorScale() * FMath::RandRange(CurrentSpawnParameters->MinSizeDelta, CurrentSpawnParameters->MaxSizeDelta));
-        const float Offset = FMath::RandRange(CurrentSpawnParameters->MinHeightDelta, CurrentSpawnParameters->MaxHeightDelta);
+        Obstacle->SetActorScale3D(Obstacle->GetActorScale() * FMath::RandRange(ObstacleData.MinSizeDelta, ObstacleData.MaxSizeDelta));
+        const float Offset = FMath::RandRange(ObstacleData.MinHeightDelta, ObstacleData.MaxHeightDelta);
         Obstacle->SetRadius(GroundRadius);
         Obstacle->SetHeightOffset(Offset);
-        Obstacle->SetSpeed(CurrentSpawnParameters->MovementSpeed);
+        Obstacle->SetSpeed(ObstacleData.MovementSpeed);
     }
 }
 
