693f6cab6f92216d0bc179f9bc0196994e60cc0d
Sashovskiy <oleksandr126@gmail.com>
2021-01-31 20:01:31 +0300

    partial start level routine

--- Source/Sharden/Obstacles/Obstacle.h
+++ Source/Sharden/Obstacles/Obstacle.h
@@ -13,8 +13,22 @@ class SHARDEN_API AObstacle : public AActor
 public:
     AObstacle();
 
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE(FEndWhipeInteract);
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, BlueprintAssignable, Category = Event)
+    FEndWhipeInteract EndWhipeInteract;
+
+    UFUNCTION(BlueprintCallable)
+    void OnSendDiraction(const FVector& Diraction);
+    UFUNCTION(BlueprintCallable)
+    void ResetCanInteract();
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Event)
+    bool Destructible = true;
+
 protected:
     virtual void BeginPlay() override;
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void GetWhipDiraction(const FVector& Diraction);
 
 public:
     virtual void Tick(float DeltaTime) override;
@@ -35,4 +49,6 @@ private:
     float Speed = 1.0f;
     float Radius = 0.0f;
     float HeightOffset = 0.0f;
+
+    bool CanInteract = true;
 };
